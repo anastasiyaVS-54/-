01c7acc496f1e5bc1810384585984343d1d1b29a
unknown <ketonchick@gmail.com>
2023-10-16 01:13:24 +0300

    Добавила папку task13

---
+++ task13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+// решение через получения
+System.Console.WriteLine("Введите число:  ");
+string userNumber = Console.ReadLine();
+
+
+int lengthfString = userNumber.Length;
+
+Console.WriteLine($"Длина строки: {lengthfString}");
+
+if(lengthfString >= 3)
+{
+    System.Console.WriteLine(userNumber[2]);
+}
+else
+{
+    System.Console.WriteLine("NO");
+}

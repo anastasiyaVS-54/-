52de750cfcc3230f4e75617824087d20f29d011f
unknown <ketonchick@gmail.com>
2023-10-16 01:13:10 +0300

    Добавила папку task11

---
+++ task11/Program.cs
@@ -0,0 +1,13 @@
+﻿//Программа, которая выводит случайное трёхзначное число и удаляет вторую цифру из этого числа.\\
+
+int number = new Random().Next(100, 1000);
+
+System.Console.WriteLine(number);
+
+int one = number / 100;
+int two = number % 10;
+
+
+
+System.Console.Write(one);
+System.Console.Write(two);

6029acfed999c0359d3c8343e1b3f8a989a9b1c0
unknown <ketonchick@gmail.com>
2023-10-16 01:12:55 +0300

    Добавила папку task10

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number / 10;
+int result2 = result % 10;
+System.Console.WriteLine(result2);

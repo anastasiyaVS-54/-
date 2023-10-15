a14244fad86e5128819560e595fc4b0c3b5a514f
unknown <ketonchick@gmail.com>
2023-10-16 01:12:20 +0300

    Добавила папку task07

---
+++ task07/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает последнюю цифру этого числа.
+
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+System.Console.WriteLine("Введите число: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+int reslt = userNumber1 % 10;
+
+System.Console.WriteLine(reslt);

9b4503898e76d920ab355670009e3fa14356639f
unknown <ketonchick@gmail.com>
2023-10-16 00:49:43 +0300

    Добавила папку task00

---
+++ task00/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Введите число: "); // cw - быстрый вызов Console.WriteLine
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = userNumber * userNumber;
+
+System.Console.WriteLine(result);
+System.Console.WriteLine(userNumber * userNumber);

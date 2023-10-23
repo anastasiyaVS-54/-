c905568f8a46cfe6a5eb570e54ce802120015e4d
unknown <ketonchick@gmail.com>
2023-10-23 15:24:25 +0300

    Задача 05

--- task05/Program.cs
+++ task05/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+System.Console.WriteLine("Введите по очереди три числа: ");
+int a = Convert.ToInt32(Console.ReadLine()); // 5
+int b = Convert.ToInt32(Console.ReadLine()); // 6
+int c = Convert.ToInt32(Console.ReadLine()); // 7
+
+int max = a;
+
+if(b > max && b > c)
+{
+    max = b;
+    System.Console.WriteLine(max);
+}
+else if(c > max && c > b)
+{
+    max = c;
+    System.Console.WriteLine(max);
+}
+else
+{
+    max = a;
+    System.Console.WriteLine(max);
+}

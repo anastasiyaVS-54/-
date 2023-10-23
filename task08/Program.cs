ed4576a3132883cb48407b7d716869822c97cea3
unknown <ketonchick@gmail.com>
2023-10-23 15:26:13 +0300

    Задача 08

---
+++ task08/Program.cs
@@ -0,0 +1,14 @@
+﻿// на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), 
+// разделеные знаком табуляции.
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for(int count = 1; count <= number; count++)
+{
+    if(count % 2 == 0)
+    {
+        int res = count;
+        System.Console.Write(res + "    ");
+    }
+}

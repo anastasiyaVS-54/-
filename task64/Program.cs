77fa6a4f4811a394c27c110d7c0cdde013538820
unknown <ketonchick@gmail.com>
2023-11-23 02:55:02 +0300

    task64

--- task64/Program.cs
+++ task64/Program.cs
@@ -2,4 +2,42 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Clear();
+// Console.Clear();
+
+using System;
+
+class NaturalNumbersProgram
+{
+    static void Main()
+    {
+        NaturalNumbersProgram program = new NaturalNumbersProgram();
+        program.Run();
+    }
+
+    void Run()
+    {
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n > 0)
+        {
+            Console.WriteLine("Результат: " + GetNaturalNumbers(n));
+        }
+        else
+        {
+            Console.WriteLine("N должно быть положительным числом.");
+        }
+    }
+
+    string GetNaturalNumbers(int n)
+    {
+        if (n == 1)
+        {
+            return "1";
+        }
+        else
+        {
+            return n + ", " + GetNaturalNumbers(n - 1);
+        }
+    }
+}
